735caef7e67b3469668e733424ebe7cf5a224672
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2024-01-04 11:26:36 +0100

    Create example2.py

---
+++ llmana2-prompting/example2.py
@@ -0,0 +1,15 @@
+# load the large language model file
+from llama_cpp import Llama
+LLM = Llama(model_path="./llama-2-7b-chat.ggmlv3.q8_0.bin",
+              n_ctx = 2048,            # context window size
+              n_gpu_layers = 1,        # enable GPU
+              use_mlock = True)        # enable memory lock so not swap
+
+# create a text prompt
+prompt = "Q: What are the names of the days of the week? A:"
+
+# generate a response (takes several seconds)
+output = LLM(prompt)
+
+# display the response
+print(output["choices"][0]["text"])
