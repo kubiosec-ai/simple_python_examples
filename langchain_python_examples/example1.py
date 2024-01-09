9d78562b7e1e3579a41b2b49435ec2a43a0077bb
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2024-01-09 22:22:36 +0100

    Update example1.py

--- langchain_python_examples/example1.py
+++ langchain_python_examples/example1.py
@@ -1,10 +1,8 @@
-from langchain_openai import OpenAI
+from langchain_openai import ChatOpenAI 
 from langchain.agents import initialize_agent
-# from langchain.agents import load_tools
 from langchain.agents import AgentType
 from langchain.agents import AgentExecutor
 from langchain_experimental.tools import PythonREPLTool
-# from langchain_experimental.utilities import PythonREPL
 
 import os
 
@@ -15,11 +13,11 @@ openai_api_key = os.getenv('OPENAI_API_KEY')
 # tools = load_tools(["python_repl"])
 tools = [PythonREPLTool()]
 
-llm = OpenAI(temperature=0., model="text-davinci-003")
+# Only certain models support this
+llm = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0)
 
 agent = initialize_agent (
         tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
 )
 
 agent.run("Can you multiply 5 and 6 and 8 and take sqrt of the result?")
-
