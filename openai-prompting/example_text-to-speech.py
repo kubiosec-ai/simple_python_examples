acb11ced5226d775c7cd380a69e77c8446dce8a2
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2024-01-09 15:51:06 +0100

    Update example_text-to-speech.py

--- openai-prompting/example_text-to-speech.py
+++ openai-prompting/example_text-to-speech.py
@@ -6,25 +6,21 @@ openai = OpenAI()
 
 speech_file_path = Path(__file__).parent / "speech.mp3"
 
-def main() -> None:
-    # Create text-to-speech audio file
-    response = openai.audio.speech.create(
-        model="tts-1", voice="alloy", input="DEVSECOPS and AI are the new way forward securing apps"
+# Create text-to-speech audio file
+response = openai.audio.speech.create(
+    model="tts-1", voice="alloy", input="DEVSECOPS and AI are the new way forward securing apps"
     )
 
-    response.stream_to_file(speech_file_path)
+response.stream_to_file(speech_file_path)
 
-    # Create transcription from audio file
-    transcription = openai.audio.transcriptions.create(model="whisper-1", file=speech_file_path)
-    print(transcription.text)
+# Create transcription from audio file
+transcription = openai.audio.transcriptions.create(model="whisper-1", file=speech_file_path)
+print(transcription.text)
 
-    # Create translation from audio file
-    translation = openai.audio.translations.create(
-        model="whisper-1",
-        file=speech_file_path,
+# Create translation from audio file
+translation = openai.audio.translations.create(
+    model="whisper-1",
+    file=speech_file_path,
     )
-    print(translation.text)
 
-
-if __name__ == "__main__":
-    main()
+print(translation.text)
