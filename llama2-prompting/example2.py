06d76699d294447362714b677fbc926f503d9d6f
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2024-01-09 17:50:11 +0100

    Update example2.py

--- llama2-prompting/example2.py
+++ llama2-prompting/example2.py
@@ -1,15 +1,15 @@
-# load the large language model file
 from llama_cpp import Llama
-LLM = Llama(model_path="./models/llama-2-7b-chat.ggmlv3.q8_0.bin",
-              n_ctx = 2048,            # context window size
-              n_gpu_layers = 1,        # enable GPU
-              use_mlock = True)        # enable memory lock so not swap
+
+# Initialize the Llama instance with the path to the downloaded model
+llm = Llama(model_path="./models/llama-2-7b-chat.Q8_0.gguf",
+            n_ctx = 2048,            # context window size
+            )        
 
 # create a text prompt
 prompt = "Q: What are the names of the days of the week? A:"
 
 # generate a response (takes several seconds)
-output = LLM(prompt)
+output = llm(prompt)
 
 # display the response
 print(output["choices"][0]["text"])
