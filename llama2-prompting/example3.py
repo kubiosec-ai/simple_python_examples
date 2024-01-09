5918ea092bac165f1b46f879eb5a415346bb574d
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2024-01-09 17:43:37 +0100

    Update example3.py

--- llama2-prompting/example3.py
+++ llama2-prompting/example3.py
@@ -1,7 +1,7 @@
 from llama_cpp import Llama
 
 # Initialize the Llama instance with the path to the downloaded model
-llm = Llama(model_path="../../Dropbox/dev/dev_llm_ai/models/llama-2-7b-chat.Q8_0.gguf")
+llm = Llama(model_path="./models/llama-2-7b-chat.Q8_0.gguf")
 
 
 output = llm("what is a kubernetes pod", 
