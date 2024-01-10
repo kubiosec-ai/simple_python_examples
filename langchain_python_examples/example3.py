6e16342cd1702768abe173a352380486e4870bd9
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2024-01-10 22:19:51 +0100

    Create example3.py

---
+++ langchain_python_examples/example3.py
@@ -0,0 +1,17 @@
+from langchain.agents import AgentType, initialize_agent
+from langchain_openai import ChatOpenAI
+from langchain.tools import ShellTool
+
+shell_tool = ShellTool()
+llm = ChatOpenAI(temperature=0)
+
+shell_tool.description = shell_tool.description + f"args {shell_tool.args}".replace(
+    "{", "{{"
+).replace("}", "}}")
+
+self_ask_with_search = initialize_agent(
+    [shell_tool], llm, agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+)
+self_ask_with_search.run(
+    "Download the langchain.com webpage and grep for all urls. Return only a sorted list of them. Be sure to use double quotes."
+)
