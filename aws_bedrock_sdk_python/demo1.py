aef60d1593a27f9227a03f604f2aa39e96b8e766
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2024-01-27 14:11:15 +0100

    Update demo1.py

--- aws_bedrock_sdk_python/demo1.py
+++ aws_bedrock_sdk_python/demo1.py
@@ -10,17 +10,10 @@ os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
 
 
 # Update boto3.client to take into account the config
-bedrock = boto3.client(service_name="bedrock")
 bedrock_runtime = boto3.client(service_name="bedrock-runtime")
-
-models_list = bedrock.list_foundation_models()
-
-model_json = json.dumps(models_list)  # models_list is  Python dictonary that needs converting to json 
-print(model_json)
 
 
 # Make sure your account is authorized to use the models
-
 prompt_data = """Command: Write me a blog about making strong business decisions as a leader.
 
 Blog:
